43c72c285a46327e89a25998581da8ea1a00cb8b
Rafi Kamal <rafikamal93@gmail.com>
2020-03-02 19:09:31 -0800

    Update disassembler to handle INX instruction.

--- src/disassembler.c
+++ src/disassembler.c
@@ -9,7 +9,7 @@ char* getLittleIndian2HexBytes(FILE* fp) {
     return hexBytes;
 }
 
-char* getLxiRegisterPair(int opCode) {
+char *getLxiInxRegisterPair(int opCode) {
     switch ((opCode & 0x30) >> 4) {
         case 0b00:
             return "B";
@@ -44,23 +44,35 @@ int main(int argc, char **argv) {
     FILE *binaryFile = fopen(binaryFileName, "rb");
     int opCode;
     while ((opCode = fgetc(binaryFile)) != EOF) {
+        // OxOO
         if (opCode == 0x00) {
             // No operation
             printf("NOP");
-        } else if ((opCode & 0xCF) == 0x01) {
+        }
+            // 0x01, 0x11, 0x21, 0x31
+        else if ((opCode & 0xCF) == 0x01) {
             // Format: LXI rp, data
             // rp can be B, D, H, or SP
             // data is a 16-bit quantity.
             // Loads immediate data into the register pair.
             // The higher 8 bits of the immediate data is loaded into the first register of the pair (e.g. C),
             // while the lower 8 bits of the immediate data is loaded into the second register of the pair (e.g. D).
-            printf("LXI %s, #$%s", getLxiRegisterPair(opCode), getLittleIndian2HexBytes(binaryFile));
-        } else if ((opCode & 0xEF) == 0x02) {
+            printf("LXI %s, #$%s", getLxiInxRegisterPair(opCode), getLittleIndian2HexBytes(binaryFile));
+        }
+            // 0x02, 0x12
+        else if ((opCode & 0xEF) == 0x02) {
             // Format: STAX rp
             // rp can be B or C.
             // Stores the content of the accumulator to the memory location addressed by the registers B and C,
             // or C and D.
             printf("STAX %s", getStaxLdaxRegisterPair(opCode));
+        }
+            // 0x03, 0x13, 0x23, 0x33
+        else if ((opCode & 0xCF) == 0x03) {
+            // Format: INX rp
+            // rp can be B, D, H, or SP
+            // Increments the 16 bit data held in the specified register by one.
+            printf("INX %s", getLxiInxRegisterPair(opCode));
         }
         printf("\n");
     }
