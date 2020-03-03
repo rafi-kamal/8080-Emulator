2ed23043c0c221255bee47efd681c19aef6c5eeb
Rafi Kamal <rafikamal93@gmail.com>
2020-03-02 19:31:54 -0800

    Update disassembler to handle INR instruction.

--- src/disassembler.c
+++ src/disassembler.c
@@ -33,6 +33,30 @@ char *getStaxLdaxRegisterPair(int opCode) {
     }
 }
 
+char *getInrDcrRegister(int opCode) {
+    switch ((opCode & 0x38) >> 3) {
+        case 0:
+            return "B";
+        case 1:
+            return "C";
+        case 2:
+            return "D";
+        case 3:
+            return "E";
+        case 4:
+            return "H";
+        case 5:
+            return "L";
+        case 6:
+            return "M";
+        case 7:
+            return "A";
+        default:
+            fprintf(stderr, "Invalid opcode %02X", opCode);
+            exit(1);
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "USAGE: program FILE");
@@ -48,8 +72,7 @@ int main(int argc, char **argv) {
         if (opCode == 0x00) {
             // No operation
             printf("NOP");
-        }
-            // 0x01, 0x11, 0x21, 0x31
+        } // 0x01, 0x11, 0x21, 0x31
         else if ((opCode & 0xCF) == 0x01) {
             // Format: LXI rp, data
             // rp can be B, D, H, or SP
@@ -58,21 +81,27 @@ int main(int argc, char **argv) {
             // The higher 8 bits of the immediate data is loaded into the first register of the pair (e.g. C),
             // while the lower 8 bits of the immediate data is loaded into the second register of the pair (e.g. D).
             printf("LXI %s, #$%s", getLxiInxRegisterPair(opCode), getLittleIndian2HexBytes(binaryFile));
-        }
-            // 0x02, 0x12
+        } // 0x02, 0x12
         else if ((opCode & 0xEF) == 0x02) {
             // Format: STAX rp
             // rp can be B or C.
             // Stores the content of the accumulator to the memory location addressed by the registers B and C,
             // or C and D.
             printf("STAX %s", getStaxLdaxRegisterPair(opCode));
-        }
-            // 0x03, 0x13, 0x23, 0x33
+        } // 0x03, 0x13, 0x23, 0x33
         else if ((opCode & 0xCF) == 0x03) {
             // Format: INX rp
             // rp can be B, D, H, or SP
             // Increments the 16 bit data held in the specified register by one.
             printf("INX %s", getLxiInxRegisterPair(opCode));
+        } // 0x04, 0x0C, 0x14, 0x1C, 0x24, 0x2C, 0x34, 0x3C
+        else if ((opCode & 0xC7) == 0x04) {
+            // Format: INR reg
+            // reg can be B, C, D, E, H, L, M (memory), A
+            // Increments the specified register or memory location by one.
+            // If a memory reference is specified (INR M), then the memory byte addressed by H and L registers
+            // is operated upon.
+            printf("INR %s", getInrDcrRegister(opCode));
         }
         printf("\n");
     }
