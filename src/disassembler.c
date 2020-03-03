76433b052f1119fd2541d5ad8823998da1cc5241
Rafi Kamal <rafikamal93@gmail.com>
2020-03-02 18:57:48 -0800

    Update disassembler to handle STAX instruction. Also fix a bug in LXI, as well as add comments for existing instructions.

--- src/disassembler.c
+++ src/disassembler.c
@@ -12,16 +12,24 @@ char* getLittleIndian2HexBytes(FILE* fp) {
 char* getLxiRegisterPair(int opCode) {
     switch ((opCode & 0x30) >> 4) {
         case 0b00:
-            return "BC";
+            return "B";
         case 0b01:
-            return "DE";
+            return "D";
         case 0b10:
-            return "HL";
+            return "H";
         case 0b11:
             return "SP";
         default:
             fprintf(stderr, "Invalid opcode %02X", opCode);
             exit(1);
+    }
+}
+
+char *getStaxLdaxRegisterPair(int opCode) {
+    if (opCode & 0x10) {
+        return "D";
+    } else {
+        return "B";
     }
 }
 
@@ -31,15 +39,28 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char* binaryFileName = argv[1];
+    char *binaryFileName = argv[1];
 
-    FILE* binaryFile = fopen(binaryFileName, "rb");
+    FILE *binaryFile = fopen(binaryFileName, "rb");
     int opCode;
     while ((opCode = fgetc(binaryFile)) != EOF) {
         if (opCode == 0x00) {
+            // No operation
             printf("NOP");
         } else if ((opCode & 0xCF) == 0x01) {
+            // Format: LXI rp, data
+            // rp can be B, D, H, or SP
+            // data is a 16-bit quantity.
+            // Loads immediate data into the register pair.
+            // The higher 8 bits of the immediate data is loaded into the first register of the pair (e.g. C),
+            // while the lower 8 bits of the immediate data is loaded into the second register of the pair (e.g. D).
             printf("LXI %s, #$%s", getLxiRegisterPair(opCode), getLittleIndian2HexBytes(binaryFile));
+        } else if ((opCode & 0xEF) == 0x02) {
+            // Format: STAX rp
+            // rp can be B or C.
+            // Stores the content of the accumulator to the memory location addressed by the registers B and C,
+            // or C and D.
+            printf("STAX %s", getStaxLdaxRegisterPair(opCode));
         }
         printf("\n");
     }
